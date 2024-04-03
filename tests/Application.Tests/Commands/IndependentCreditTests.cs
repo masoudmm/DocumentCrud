ca15ca7bf7dba174ab02bfe2737afcc52530b03c
Masoud <masoud.m@elamo.com.au>
2024-04-03 17:50:07 +0330

    + Added some sample unit tests for commands, validators and validator behaviors

---
+++ tests/Application.Tests/Commands/IndependentCreditTests.cs
@@ -0,0 +1,68 @@
+using AutoMapper;
+using DocumentCrud.Application.Dtos;
+using DocumentCrud.Application.Features.Commands.Create;
+using DocumentCrud.Domain.BaseEntities;
+using DocumentCrud.Domain.Contracts.Persistence;
+using DocumentCrud.Domain.Contracts.Persistence.Repositories;
+using DocumentCrud.Domain.CreditAggregate;
+using FluentAssertions;
+using Moq;
+
+namespace Application.Tests.Commands
+{
+    public class IndependentCreditTests
+    {
+        [Fact]
+        public async Task Create_Independent_Credit_Command_Should_Succeed()
+        {
+            // Arrange
+            var mockMapper = new Mock<IMapper>();
+            var mockUnitOfWork = new Mock<IUnitOfWork>();
+            var mockIndependentCreditNoteRepository = new Mock<IIndependentCreditRepository>();
+
+            var command = new CreateIndependentCreditCommand(
+                "1234567890",
+                "123456icr1",
+                AccountingDocumentStatus.WaitingForApproval,
+                100m);
+
+            var createdCredit = IndependentCreditNote.CreateNew(command.Number,
+                command.ExternalCreditNumber,
+                command.Status,
+                command.TotalAmount);
+
+            mockUnitOfWork.Setup(uow => uow.IndependentCreditNotes)
+                .Returns(mockIndependentCreditNoteRepository.Object);
+
+            mockIndependentCreditNoteRepository.Setup(repo => repo.AddAsync(It.IsAny<IndependentCreditNote>()))
+                .Returns(Task.CompletedTask)
+                .Callback<IndependentCreditNote>(creditNote =>
+                {
+                    creditNote.Should().NotBeNull();
+                    creditNote.Number.Should().Be(command.Number);
+                });
+
+            mockUnitOfWork.Setup(uow => uow.CommitAsync()).Returns(Task.FromResult(0));
+
+            var dto = new DocumentDto
+            {
+                Number = command.Number,
+                ExternalNumber = command.ExternalCreditNumber,
+                Status = command.Status,
+                TotalAmount = command.TotalAmount,
+            };
+            mockMapper.Setup(mapper => mapper.Map<DocumentDto>(It.IsAny<IndependentCreditNote>()))
+                .Returns(dto);
+
+            var handler = new CreateIndependentCreditCommandHandler(mockMapper.Object, mockUnitOfWork.Object);
+
+            // Act
+            var result = await handler.Handle(command, CancellationToken.None);
+
+            // Assert
+            result.Should().BeEquivalentTo(dto);
+            mockIndependentCreditNoteRepository.Verify(repo => repo.AddAsync(It.IsAny<IndependentCreditNote>()), Times.Once);
+            mockUnitOfWork.Verify(uow => uow.CommitAsync(), Times.Once);
+        }
+    }
+}
---
+++ tests/Application.Tests/Validators/CreateInvoiceCommandValidatorTests.cs
@@ -0,0 +1,66 @@
+﻿using DocumentCrud.Application.Dtos;
+using DocumentCrud.Application.Features.Commands.Create;
+using DocumentCrud.Domain.BaseEntities;
+using FluentValidation.TestHelper;
+
+namespace Application.Tests.Validators;
+
+public class CreateInvoiceCommandValidatorTests
+{
+    private readonly CreateInvoiceCommandValidator _validator;
+
+    public CreateInvoiceCommandValidatorTests()
+    {
+        _validator = new CreateInvoiceCommandValidator();
+    }
+
+    [Fact]
+    public void Validate_ShouldHaveError_WhenNumberIsInvalid()
+    {
+        // Arrange
+        var command = new CreateInvoiceCommand("123456789000000",
+            "123456inv1",
+            AccountingDocumentStatus.WaitingForApproval,
+            100,
+            Enumerable.Empty<DependentCreditNoteDto>().ToList());
+
+        // Act
+        var result = _validator.TestValidate(command);
+
+        // Assert
+        result.ShouldHaveValidationErrorFor(c => c.Number);
+    }
+
+    [Fact]
+    public void Validate_ShouldHaveError_WhenExternalCreditNumberIsInvalid()
+    {
+        // Arrange
+        var command = new CreateInvoiceCommand("1234567890",
+            "123456inv111111",
+            AccountingDocumentStatus.WaitingForApproval,
+            100,
+            Enumerable.Empty<DependentCreditNoteDto>().ToList());
+        // Act
+        var result = _validator.TestValidate(command);
+
+        // Assert
+        result.ShouldHaveValidationErrorFor(c => c.ExternalInvoiceNumber);
+    }
+
+    [Fact]
+    public void Validate_ShouldNotHaveError_WhenCommandIsValid()
+    {
+        // Arrange
+        var command = new CreateInvoiceCommand("1234567890",
+            "1234567890",
+            AccountingDocumentStatus.WaitingForApproval, 
+            100,
+            Enumerable.Empty<DependentCreditNoteDto>().ToList());
+
+        // Act
+        var result = _validator.TestValidate(command);
+
+        // Assert
+        result.ShouldNotHaveAnyValidationErrors();
+    }
+}
